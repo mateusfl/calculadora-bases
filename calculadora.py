6395c2993e793c8f1f753f42a23517fba6503f4e
mauri <mateusfelipern@gmail.com>
2025-04-25 08:00:28 -0300

    Adiciona verificação de base muito grande

--- calculadora.py
+++ calculadora.py
@@ -6,13 +6,27 @@ operacao = input(
     "Qual operação você deseja fazer? ('s' para soma; 'm' para multiplicação; 'c' para conversão simples) - "
 )
 
-if operacao == 'c':
+if operacao == "c":
     n = input("Digite o valor para converter: ")
-    b1, b2 = map(int, input("Digite a base de origem e a base de destino separadas por espaço: ").split(" "))
+    b1, b2 = map(
+        int,
+        input(
+            "Digite a base de origem e a base de destino separadas por espaço: "
+        ).split(" "),
+    )
+
+    if b2 > 12:
+        print("ERRO: Ainda não é possível realizar operações com bases maiores que 12. ")
+        exit()
+
 else:
     base = int(input("Em qual base você deseja realizar a operação? - "))
     n1 = input("Digite o primeiro valor da operação: ")
     n2 = input("Digite o segundo valor da operação: ")
+
+    if base > 12:
+        print("ERRO: Ainda não é possível realizar operações com bases maiores que 12. ")
+        exit()
 
 print("-" * 30)
 
@@ -40,6 +54,8 @@ match operacao:
         print(f".O resultado em octal é {decimal_para_base(str(mult), 8)}")
         print(f".O resultado em hexadecimal é {decimal_para_base(str(mult), 16)}")
     case "c":
-       print(f"O valor {n}({b1}) é igual a {decimal_para_base(str(base_para_decimal(n, b1)), b2)}({b2})")
+        print(
+            f"O valor {n}({b1}) é igual a {decimal_para_base(str(base_para_decimal(n, b1)), b2)}({b2})"
+        )
     case _:
         print("Operação inválida")
