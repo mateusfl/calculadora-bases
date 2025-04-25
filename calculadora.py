f72445a698c319a7aedd40565afcd140fd5835c6
mauri <mateusfelipern@gmail.com>
2025-04-25 07:26:01 -0300

    Adiciona conversão simples entre bases

--- calculadora.py
+++ calculadora.py
@@ -3,13 +3,18 @@ from decimal_para_base import decimal_para_base
 
 
 operacao = input(
-    "Qual operação você deseja fazer? ('s' para soma; 'm' para multiplicação) - "
+    "Qual operação você deseja fazer? ('s' para soma; 'm' para multiplicação; 'c' para conversão simples) - "
 )
-base = int(input("Em qual base você deseja realizar a operação? - "))
-n1 = input("Digite o primeiro valor da operação: ")
-n2 = input("Digite o segundo valor da operação: ")
-print("-" * 30)
 
+if operacao == 'c':
+    n = input("Digite o valor para converter: ")
+    b1, b2 = map(int, input("Digite a base de origem e a base de destino separadas por espaço: ").split(" "))
+else:
+    base = int(input("Em qual base você deseja realizar a operação? - "))
+    n1 = input("Digite o primeiro valor da operação: ")
+    n2 = input("Digite o segundo valor da operação: ")
+
+print("-" * 30)
 
 match operacao:
     case "s":
@@ -17,8 +22,7 @@ match operacao:
         n2_decimal = base_para_decimal(n2, base)
         soma = n1_decimal + n2_decimal
         print(
-            f"-> {n1} somado com {n2} na base {base} é igual a {decimal_para_base(str(soma), base)}",
-            end="\n",
+            f"-> {n1} somado com {n2} na base {base} é igual a {decimal_para_base(str(soma), base)}"
         )
         print(f".O resultado em decimal é {round(soma, 4)}")
         print(f".O resultado em binário é {decimal_para_base(str(soma), 2)}")
@@ -29,12 +33,13 @@ match operacao:
         n2_decimal = base_para_decimal(n2, base)
         mult = n1_decimal * n2_decimal
         print(
-            f"-> {n1} multiplicado por {n2} na base {base} é igual a {decimal_para_base(str(mult), base)}",
-            end="\n",
+            f"-> {n1} multiplicado por {n2} na base {base} é igual a {decimal_para_base(str(mult), base)}"
         )
         print(f".O resultado em decimal é {round(mult, 4)}")
         print(f".O resultado em binário é {decimal_para_base(str(mult), 2)}")
         print(f".O resultado em octal é {decimal_para_base(str(mult), 8)}")
         print(f".O resultado em hexadecimal é {decimal_para_base(str(mult), 16)}")
+    case "c":
+       print(f"O valor {n}({b1}) é igual a {decimal_para_base(str(base_para_decimal(n, b1)), b2)}({b2})")
     case _:
         print("Operação inválida")
